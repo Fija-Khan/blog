af9a1e0dc5bba3626cb1cf65fb08f03bd39a4770
Pathan Fija Sattar <fijapathan9763@gmail.com>
2025-10-08 12:32:18 +0530

    myblog app folder is add

---
+++ myblog/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog
+from .forms import BlogForm
+
+
+def blog_list(request):
+    blogs = Blog.objects.all().order_by('-created_at')
+    return render(request, 'myblog/blog_list.html', {'blogs': blogs})
+
+def blog_detail(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    blog.views += 1
+    blog.save()
+    return render(request, 'myblog/blog_detail.html', {'blog': blog})
+
+def blog_create(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm()
+    return render(request, 'myblog/blog_form.html', {'form': form, 'operation': 'Add'})
+
+def blog_edit(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method == "POST":
+        form = BlogForm(request.POST, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'myblog/blog_form.html', {'form': form, 'operation': 'Edit'})
+
+def blog_delete(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method == "POST":
+        blog.delete()
+        return redirect('blog_list')
+    return render(request, 'myblog/blog_delete.html', {'blog': blog})
